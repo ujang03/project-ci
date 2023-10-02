a92436ba7d90aca7b8fd4b66c61ec01012d33315
Ujang IT <Ujang@STAFF.PRESUNIV.COM>
2023-10-02 11:41:40 +0700

    berkas rahasia

---
+++ application/controllers/Auth.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+
+    public function __construct()
+    {
+       parent::__construct();
+       $this->load->library('form_validation');
+    }
+    public function index()
+    {
+        $data['title']='Login User';
+        $this->load->view('templates/auth_header', $data);
+        $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
+    }
+    public function registration()
+    {
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]');
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]');
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password2]');
+
+        if($this->form_validation->run() == false) {
+           $data['title']='Resgister User';
+           $this->load->view('templates/auth_header', $data);
+           $this->load->view('auth/registration');
+           $this->load->view('templates/auth_footer');
+        } else {
+            echo'data behasil ditambahkan!';
+        }
+    }
+}
